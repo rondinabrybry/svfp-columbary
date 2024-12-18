85c6f0bc5fcc15b339c672a354e7e83ba955f7a7
backend.dude <71830788+rondinabrybry@users.noreply.github.com>
2024-12-18 14:12:26 +0800

    sadsa

--- database/migrations/2024_12_09_062714_create_columbary_slots_table.php
+++ database/migrations/2024_12_09_062714_create_columbary_slots_table.php
@@ -10,9 +10,12 @@ class CreateColumbarySlotsTable extends Migration
     {
         Schema::create('columbary_slots', function (Blueprint $table) {
             $table->id();
-            $table->integer('slot_number')->unique();
+            $table->integer('slot_number');
+            $table->index('slot_number'); 
             $table->integer('floor_number');
+            $table->index('floor_number');
             $table->integer('vault_number');
+            $table->index('vault_number');
             $table->enum('status', ['Available', 'Reserved', 'Sold', 'Not Available'])->default('Available');
             $table->decimal('price', 10, 2);
             $table->timestamps();
